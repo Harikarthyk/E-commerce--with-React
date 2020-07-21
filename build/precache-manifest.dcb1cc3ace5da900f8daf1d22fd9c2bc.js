a4b27a469314960ea4b2a4622669f9bbab41a904
Hari_karthyk <54505967+Harikarthyk@users.noreply.github.com>
2020-07-21 11:46:57 +0530

    Add files via upload

---
+++ build/precache-manifest.dcb1cc3ace5da900f8daf1d22fd9c2bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc641b9a438598af89aed8cda5f8ffc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d21b6ea54b3618af6a7",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "7e8954a0df95d9874f6d",
+    "url": "/static/css/main.3f9a85e2.chunk.css"
+  },
+  {
+    "revision": "8d21b6ea54b3618af6a7",
+    "url": "/static/js/2.1ce516bc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.1ce516bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e8954a0df95d9874f6d",
+    "url": "/static/js/main.100e5439.chunk.js"
+  },
+  {
+    "revision": "8f67dd6eb413eafc8a96",
+    "url": "/static/js/runtime-main.e8cb9fc9.js"
+  }
+]);
